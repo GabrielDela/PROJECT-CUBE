89871009b2da77646a781e82e71e8a01d2d3c457
Gabriel <gabrieldelahaye76680@gmail.com>
2022-02-03 17:53:38 +0100

    MAJ VERIF USER EXISTS

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -7,39 +7,48 @@ var router = express.Router();
 
 const User = require('../models/user');
 
-function generateAccessToken(user){
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1800s'});
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1800s' });
 }
 
-function generateRefreshToken(user){
-    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1y'});
+function generateRefreshToken(user) {
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1y' });
 }
 
 router.post('/refresh-token', authenticateToken, function (req, res) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    
-    if(!token){
+
+    if (!token) {
         return res.sendStatus(401);
     }
 
     jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if(err){
+        if (err) {
             return res.sendStatus(401);
         }
 
-        //TODO : check en bdd que le user a toujour les droits et qu'il existe toujours
-        delete user.iat;
-        delete user.exp;
-        var refreshedToken = generateAccessToken(user);
-        res.status(200).json({
-            accessToken: refreshedToken
+        User.findOne({ email: user.email }).then(result => {
+            if (user.password == result.password) {
+                delete user.iat;
+                delete user.exp;
+                var refreshedToken = generateAccessToken(user);
+                res.status(200).json({
+                    accessToken: refreshedToken
+                });
+            }
+            else {
+                res.status(401).json("Invalid credentials");
+            }
+        }).catch((error) => {
+            console.log(error);
+            res.status(400).json({ error });
         });
     });
 });
 
 router.post('/login', function (req, res) {
-    if(req.body.email == null || req.body.password == null){
+    if (req.body.email == null || req.body.password == null) {
         res.status(401).json("Invalid credentials");
     }
     const password = sha256(req.body.password);
@@ -47,7 +56,7 @@ router.post('/login', function (req, res) {
     console.log(req.body)
 
     User.findOne({ email: req.body.email }).then(user => {
-        if(user.password == password){
+        if (user.password == password) {
             var accessToken = generateAccessToken(user.toJSON());
             var refreshToken = generateRefreshToken(user.toJSON());
             res.status(200).json({
@@ -55,33 +64,7 @@ router.post('/login', function (req, res) {
                 refreshToken: refreshToken
             });
         }
-        else{
-            res.status(401).json("Invalid credentials");
-        }
-    }).catch((error) => {
-        console.log(error);
-        res.status(400).json({ error });
-    });
-});
-
-router.post('/logout', function (req, res) {
-    if(req.body.email == null || req.body.password == null){
-        res.status(401).json("Invalid credentials");
-    }
-    const password = sha256(req.body.password);
-
-    console.log(req.body)
-
-    User.findOne({ email: req.body.email }).then(user => {
-        if(user.password == password){
-            var accessToken = generateAccessToken(user.toJSON());
-            var refreshToken = generateRefreshToken(user.toJSON());
-            res.status(200).json({
-                accessToken: accessToken,
-                refreshToken: refreshToken
-            });
-        }
-        else{
+        else {
             res.status(401).json("Invalid credentials");
         }
     }).catch((error) => {
@@ -91,20 +74,29 @@ router.post('/logout', function (req, res) {
 });
 
 router.get('/me', authenticateToken, function (req, res) {
-    console.log(req.user);
-    res.status(200).json(req.user);
+    User.findOne({ email: req.user.email }).then(user => {
+        if (req.user.password == user.password) {
+            res.status(200).json(req.user);
+        }
+        else {
+            res.status(401).json("Invalid credentials");
+        }
+    }).catch((error) => {
+        console.log(error);
+        res.status(400).json({ error });
+    });
 });
 
-function authenticateToken(req, res, next){
+function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if(!token){
+    if (!token) {
         return res.sendStatus(401);
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err){
+        if (err) {
             return res.sendStatus(401);
         }
         req.user = user;
